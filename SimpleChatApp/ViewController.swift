969414726ce585df765c8bf0172f515760c5fde7
堀川浩二 <>
2019-08-08 12:10:29 +0800

    first commit

---
+++ SimpleChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleChatApp
+//
+//  Created by 堀川浩二 on 2019/08/08.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
